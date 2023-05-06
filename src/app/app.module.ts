0a644ff49062e10eb1e743d428b33abfe341df50
paragchauhan98 <iamparag121298@gmail.com>
2023-05-06 12:10:45 +0530

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { firebase } from '../environments/environment';
+import { environment } from '../environments/environment';
 
 import { SignInComponent } from './registration/components/sign-in/sign-in.component';
 import { SignUpComponent } from './registration/components/sign-up/sign-up.component';
@@ -64,7 +64,7 @@ import { AuthService } from './shared/services/auth.service';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    AngularFireModule.initializeApp(firebase),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireStorageModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: true
-};
-export const firebase ={
-  apiKey: "AIzaSyAb1xw3BMyzXoo4z9eD-YZLUkz1-eeIlpQ",
-  authDomain: "stocksology-bf79a.firebaseapp.com",
-  projectId: "stocksology-bf79a",
-  storageBucket: "stocksology-bf79a.appspot.com",
-  messagingSenderId: "989271616534",
-  appId: "1:989271616534:web:60ad979bdfbbc4d6809937",
-  measurementId: "G-PC6F76DCG1"
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyAb1xw3BMyzXoo4z9eD-YZLUkz1-eeIlpQ",
+    authDomain: "stocksology-bf79a.firebaseapp.com",
+    projectId: "stocksology-bf79a",
+    storageBucket: "stocksology-bf79a.appspot.com",
+    messagingSenderId: "989271616534",
+    appId: "1:989271616534:web:60ad979bdfbbc4d6809937",
+    measurementId: "G-PC6F76DCG1"
+  },
 };
